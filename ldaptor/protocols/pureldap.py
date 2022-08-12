35db71cabb00f92dd616a7fb829772e021e8919c
Lars Esser <mail@lars-esser.de>
2022-08-12 15:16:51 +0200

    pureldap: implement LDAPIntermediateResponse

--- ldaptor/protocols/pureldap.py
+++ ldaptor/protocols/pureldap.py
@@ -1900,6 +1900,58 @@ class LDAPExtendedResponse(LDAPResult):
         return self.wrap(ret)
 
 
+class LDAPIntermediateResponse_responseName(LDAPOID):
+    _tag_class = TagClasses.CONTEXT
+    _tag = 0x00
+
+
+class LDAPIntermediateResponse_responseValue(BEROctetString):
+    _tag_class = TagClasses.CONTEXT
+    _tag = 0x01
+
+
+# IntermediateResponse ::= [APPLICATION 25] SEQUENCE {
+#         responseName     [0] LDAPOID OPTIONAL,
+#         responseValue    [1] OCTET STRING OPTIONAL }
+class LDAPIntermediateResponse(LDAPProtocolResponse, BERSequence):
+    _tag_class = TagClasses.APPLICATION
+    _tag = 0x19
+    responseName: Optional[bytes]
+    responseValue: Optional[bytes]
+
+    @classmethod
+    def from_wire(cls, content: bytes) -> "LDAPIntermediateResponse":
+        vals = cls.unwrap(content)
+        check(0 <= len(vals) <= 2)
+
+        responseName = None
+        responseValue = None
+        for unkown_tag, unkown_content in vals:
+            if unkown_tag == LDAPIntermediateResponse_responseName.tag:
+                if responseName is not None:
+                    raise ValueError
+                responseName = LDAPIntermediateResponse_responseName.from_wire(unkown_content).value
+            elif unkown_tag == LDAPIntermediateResponse_responseValue.tag:
+                if responseValue is not None:
+                    raise ValueError
+                responseValue = LDAPIntermediateResponse_responseValue.from_wire(unkown_content).value
+            else:
+                raise UnknownBERTag(unkown_tag)
+        return cls(responseName=responseName, responseValue=responseValue)
+
+    def __init__(self, responseName: bytes = None, responseValue: bytes = None):
+        self.responseName = responseName
+        self.responseValue = responseValue
+
+    def to_wire(self) -> bytes:
+        ret: List[BERBase] = []
+        if self.responseName is not None:
+            ret.append(LDAPIntermediateResponse_responseName(self.responseName))
+        if self.responseValue is not None:
+            ret.append(LDAPIntermediateResponse_responseValue(self.responseValue))
+        return self.wrap(ret)
+
+
 FILTERS: Mapping[int, Type[LDAPFilter]] = {
     LDAPFilter_not.tag: LDAPFilter_not,
     LDAPFilter_equalityMatch.tag: LDAPFilter_equalityMatch,
