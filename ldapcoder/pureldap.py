eff73487b2fd0706b93bdaa9419793d76f9cc58f
Lars Esser <mail@lars-esser.de>
2022-08-12 19:16:02 +0200

    ldapcoder/add: remove superfluous content

--- ldapcoder/pureldap.py
+++ ldapcoder/pureldap.py
@@ -179,51 +179,6 @@ class LDAPControl(BERSequence):
                 f" criticality={criticality}, controlValue={self.controlValue})")
 
 
-# AddRequest ::= [APPLICATION 8] SEQUENCE {
-#      entry           LDAPDN,
-#      attributes      AttributeList }
-class LDAPAddRequest(LDAPProtocolRequest, BERSequence):
-    _tag_class = TagClasses.APPLICATION
-    _tag = 0x08
-    entry: str
-    attributes: List[LDAPAttribute]
-
-    @classmethod
-    def from_wire(cls, content: bytes) -> "LDAPAddRequest":
-        vals = cls.unwrap(content)
-        check(len(vals) == 2)
-        entry = decode(vals[0], LDAPDN).value
-        attributes = decode(vals[1], LDAPAttributeList).value
-        return cls(entry=entry, attributes=attributes)
-
-    def __init__(self, entry: str, attributes: List[LDAPAttribute]):
-        self.entry = entry
-        self.attributes = attributes
-
-    def to_wire(self) -> bytes:
-        return self.wrap([LDAPDN(self.entry), LDAPAttributeList(self.attributes)])
-
-    def __repr__(self):
-        entry = self.entry
-        if self.tag == self.__class__.tag:
-            return self.__class__.__name__ + "(entry={}, attributes={})".format(
-                repr(entry),
-                repr(self.attributes),
-            )
-        else:
-            return self.__class__.__name__ + "(entry=%s, attributes=%s, tag=%d)" % (
-                repr(entry),
-                repr(self.attributes),
-                self.tag,
-            )
-
-
-# AddResponse ::= [APPLICATION 9] LDAPResult
-class LDAPAddResponse(LDAPResult):
-    _tag_class = TagClasses.APPLICATION
-    _tag = 0x09
-
-
 # DelRequest ::= [APPLICATION 10] LDAPDN
 class LDAPDelRequest(LDAPProtocolRequest, LDAPString):
     _tag_class = TagClasses.APPLICATION
