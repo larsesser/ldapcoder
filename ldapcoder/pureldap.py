d19cc4334f2e2dd347f6e833f5c6c90491c9c1cb
Lars Esser <mail@lars-esser.de>
2022-08-12 19:24:53 +0200

    ldapcoder/abandon: remove superfluous content

--- ldapcoder/pureldap.py
+++ ldapcoder/pureldap.py
@@ -29,22 +29,6 @@ from ldaptor.protocols.pureber import (
 )
 
 
-# AbandonRequest ::= [APPLICATION 16] MessageID
-class LDAPAbandonRequest(LDAPProtocolRequest, LDAPMessageId):
-    _tag_class = TagClasses.APPLICATION
-    _tag = 0x10
-    needs_answer = 0
-
-    def __repr__(self):
-        if self.tag == self.__class__.tag:
-            return self.__class__.__name__ + "(id=%s)" % repr(self.value)
-        else:
-            return self.__class__.__name__ + "(id=%s, tag=%d)" % (
-                repr(self.value),
-                self.tag,
-            )
-
-
 class LDAPIntermediateResponse_responseName(LDAPOID):
     _tag_class = TagClasses.CONTEXT
     _tag = 0x00
