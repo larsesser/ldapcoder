6c0488d8c11aab28ccbfc48bbf8bc40b0f28dbab
Lars Esser <mail@lars-esser.de>
2022-08-12 19:20:35 +0200

    ldapcoder/delete: remove superfluous content

--- ldapcoder/pureldap.py
+++ ldapcoder/pureldap.py
@@ -29,28 +29,6 @@ from ldaptor.protocols.pureber import (
 )
 
 
-# DelRequest ::= [APPLICATION 10] LDAPDN
-class LDAPDelRequest(LDAPProtocolRequest, LDAPString):
-    _tag_class = TagClasses.APPLICATION
-    _tag = 0x0A
-
-    def __repr__(self):
-        entry = self.value
-        if self.tag == self.__class__.tag:
-            return self.__class__.__name__ + "(entry=%s)" % repr(entry)
-        else:
-            return self.__class__.__name__ + "(entry=%s, tag=%d)" % (
-                repr(entry),
-                self.tag,
-            )
-
-
-# DelResponse ::= [APPLICATION 11] LDAPResult
-class LDAPDelResponse(LDAPResult):
-    _tag_class = TagClasses.APPLICATION
-    _tag = 0x0B
-
-
 class LDAPModifyDNResponse_newSuperior(LDAPDN):
     _tag_class = TagClasses.CONTEXT
     _tag = 0x00
