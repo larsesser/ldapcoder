33e373870a6c0042f39a33b92565a658084c84ff
Lars Esser <mail@lars-esser.de>
2022-08-12 19:22:15 +0200

    ldapcoder/modify-dn: remove superfluous content

--- ldapcoder/pureldap.py
+++ ldapcoder/pureldap.py
@@ -29,69 +29,6 @@ from ldaptor.protocols.pureber import (
 )
 
 
-class LDAPModifyDNResponse_newSuperior(LDAPDN):
-    _tag_class = TagClasses.CONTEXT
-    _tag = 0x00
-
-
-# ModifyDNRequest ::= [APPLICATION 12] SEQUENCE {
-#      entry           LDAPDN,
-#      newrdn          RelativeLDAPDN,
-#      deleteoldrdn    BOOLEAN,
-#      newSuperior     [0] LDAPDN OPTIONAL }
-class LDAPModifyDNRequest(LDAPProtocolRequest, BERSequence):
-    _tag_class = TagClasses.APPLICATION
-    _tag = 0x0C
-
-    entry: str
-    newrdn: str
-    deleteoldrdn: bool
-    newSuperior: Optional[str]
-
-    @classmethod
-    def from_wire(cls, content: bytes) -> "LDAPModifyDNRequest":
-        vals = cls.unwrap(content)
-        check(3 <= len(vals) <= 4)
-
-        entry = decode(vals[0], LDAPDN).value
-        newrdn = decode(vals[1], LDAPRelativeDN).value
-        deleteoldrdn = decode(vals[2], BERBoolean).value
-        newSuperior = None
-        if len(vals) == 4:
-            newSuperior = decode(vals[3], LDAPModifyDNResponse_newSuperior).value
-        return cls(entry=entry, newrdn=newrdn, deleteoldrdn=deleteoldrdn, newSuperior=newSuperior)
-
-    def __init__(self, entry: str, newrdn: str, deleteoldrdn: bool, newSuperior: str = None):
-        self.entry = entry
-        self.newrdn = newrdn
-        self.deleteoldrdn = deleteoldrdn
-        self.newSuperior = newSuperior
-
-    def to_wire(self) -> bytes:
-        ret: List[BERBase] = [LDAPDN(self.entry), LDAPRelativeDN(self.newrdn), BERBoolean(self.deleteoldrdn)]
-        if self.newSuperior is not None:
-            ret.append(LDAPModifyDNResponse_newSuperior(self.newSuperior))
-        return self.wrap(ret)
-
-    def __repr__(self):
-        l = [
-            "entry=%s" % repr(self.entry),
-            "newrdn=%s" % repr(self.newrdn),
-            "deleteoldrdn=%s" % repr(self.deleteoldrdn),
-        ]
-        if self.newSuperior is not None:
-            l.append("newSuperior=%s" % repr(self.newSuperior))
-        if self.tag != self.__class__.tag:
-            l.append("tag=%d" % self.tag)
-        return self.__class__.__name__ + "(" + ", ".join(l) + ")"
-
-
-# ModifyDNResponse ::= [APPLICATION 13] LDAPResult
-class LDAPModifyDNResponse(LDAPResult):
-    _tag_class = TagClasses.APPLICATION
-    _tag = 0x0D
-
-
 # CompareRequest ::= [APPLICATION 14] SEQUENCE {
 #      entry           LDAPDN,
 #      ava             AttributeValueAssertion }
